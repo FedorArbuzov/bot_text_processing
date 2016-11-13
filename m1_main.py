60ec9961d070cf56b740944848b672b2d82755ae
Fedor Arbuzov <FPArbuzov@edu.hse.ru>
2016-11-13 16:14:01 +0300

    Add files via upload
    add files to this project

---
+++ m1_main.py
@@ -0,0 +1,214 @@
+import telebot
+import requests
+import time
+import logging
+import os
+from transliterate import translit as tr
+from telebot import types
+
+import constants
+import config
+from m1_req import main_func
+from m1_req import hello_back
+from m1_speechkit import speech_to_text
+from m1_speechkit import SpeechException
+"""
+from m2_main import M2Retrieving
+from m3_main import M3Visualizing
+"""
+API_TOKEN = config.TELEGRAM_API_TOKEN1
+bot = telebot.TeleBot(API_TOKEN)
+
+logging.basicConfig(filename='logs.log', level=50, format='%(asctime)s\t%(message)s', datefmt='%Y-%m-%d %H:%M')
+
+
+# /start command handler; send start-message to the user
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, constants.START_MSG, parse_mode='HTML')
+
+
+# /help command handler; send hello-message to the user
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.send_message(
+        message.chat.id,
+        constants.HELP_MSG,
+        parse_mode='HTML',
+        reply_markup=constants.HELP_KEYBOARD,
+        disable_web_page_preview=True)
+
+
+# /search message handler
+@bot.message_handler(commands=['search'])
+def repeat_all_messages(message):
+    # TODO: abstraction7
+    s = message.text[8:]
+    if s != '':  # if there's no text after /search, start questioning user w/ buttons
+        # parse message after the /search command and pass it further
+        s1 = main_func(s.lower())
+        asked_question = s.lower()
+        logging.critical("{}\t{}".format(message.chat.id, asked_question))
+        s_mod2 = forming_string_from_neural(s1)
+        querying_and_visualizing(message, s_mod2)
+
+
+# Text handler | PRESSING NON-INLINE BUTTONS RETURNS TEXT TOO!
+@bot.message_handler(content_types=['text'])
+def repeat_all_messages(message):
+    # check if user wants us to salute him
+    if hello_back(message.text) is not None:
+        bot.send_message(message.chat.id, hello_back(message.text))
+
+
+# inline mode handler
+@bot.inline_handler(lambda query: len(query.query) >= 0)
+def query_text(query):
+    input_message_content = query.query
+    s1 = main_func(input_message_content)
+    s_mod2 = forming_string_from_neural(s1)  # receive Module 2-friendly string format
+    print(s_mod2)
+    """
+    result_array = []
+    result = M2Retrieving.get_data(s_mod2)  # check if current user string is correct
+    if result.status is False:  # in case the string is not correct we ask user to keep typing
+        msg = types.InlineQueryResultArticle(id='0',
+                                             title='Продолжайте ввод запроса',
+                                             input_message_content=types.InputTextMessageContent(
+                                                 message_text=input_message_content + '\nЗапрос не удался😢'
+                                             ))
+        result_array.append(msg)  # Nothing works without this list, I dunno why :P
+        bot.answer_inline_query(query.id, result_array)
+
+    else:
+        logging.critical("{}\t{}".format("inline", input_message_content))
+        m3_result = M3Visualizing.create_response(query.id, result.response, result.theme, visualization=False)
+        try:
+            if m3_result.data is False:
+                msg_append_text = ': ' + constants.ERROR_NULL_DATA_FOR_SUCH_REQUEST_SHORT
+                title = 'Данных нет'
+            else:
+                msg_append_text = ':\n' + str(m3_result.number)
+                title = str(m3_result.number)
+
+            msg = types.InlineQueryResultArticle(id='1',
+                                                 title=title,
+                                                 input_message_content=types.InputTextMessageContent(
+                                                     message_text=input_message_content + msg_append_text),
+                                                 )
+            result_array.append(msg)
+
+        finally:
+            bot.answer_inline_query(query.id, result_array)
+    """
+
+# voice message handler
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+    file_info = bot.get_file(message.voice.file_id)
+    file = requests.get(
+        'https://api.telegram.org/file/bot{0}/{1}'.format(API_TOKEN, file_info.file_path))
+    try:
+        text = speech_to_text(bytes=file.content)
+        logging.critical("{}\t{}".format(message.chat.id, text))
+    except SpeechException:
+        msg = constants.ERROR_CANNOT_UNDERSTAND_VOICE
+        bot.send_message(message.chat.id, msg)
+    else:
+        s1 = main_func(text)
+        s_mod2 = forming_string_from_neural(s1)
+        querying_and_visualizing(message, s_mod2)
+
+
+def file_naming(request_string):
+    request_string = tr(request_string, 'ru', reversed=True)
+    filename = request_string.replace('null', '')
+    filename = filename.replace(',', '_')
+
+    for i in range(0, 3):
+        filename = filename.replace('__', '_')
+
+    if filename[len(filename) - 1] == '_':
+        filename = filename[:len(filename) - 1]
+
+    print(filename)
+
+    filename_svg = 'diagram_' + filename + '.svg'
+    filename_pdf = 'table_' + filename + '.pdf'
+    names = [filename_svg, filename_pdf]
+    return names
+
+
+def forming_string_from_neural(s1):
+    s_mod2 = ''
+    if s1[0] == 'расходы':
+        s_mod2 += s1[0] + ',' + s1[4] + ',' + 'null' + ',' + str(s1[2]) + ',' + str(s1[3]) + ',' + s1[1]
+    elif s1[0] == 'доходы':
+        s_mod2 += s1[0] + ',' + s1[4] + ',' + str(s1[3]) + ',' + str(s1[2]) + ',' + 'null' + ',' + s1[1]
+    elif s1[0] == 'дефицит':
+        s_mod2 += s1[0] + ',' + s1[4] + ',' + 'null' + ',' + str(s1[2]) + ',' + 'null' + ',' + s1[1]
+    return s_mod2
+
+
+def querying_and_visualizing(message, s_mod2, notify_user=True):
+    markup = types.ReplyKeyboardHide()
+    print(s_mod2)
+    """
+    try:
+        m2_result = M2Retrieving.get_data(s_mod2)
+        if m2_result.status is False:
+            bot.send_message(message.chat.id, m2_result.message, reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, constants.MSG_WE_WILL_FORM_DATA_AND_SEND_YOU, reply_markup=markup)
+            names = file_naming(s_mod2)
+            m3_result = M3Visualizing.create_response(message.chat.id, m2_result.response, m2_result.theme,
+                                                      filename_svg=names[0], filename_pdf=names[1])
+            if m3_result.data is False:
+                bot.send_message(message.chat.id, constants.ERROR_NULL_DATA_FOR_SUCH_REQUEST_LONG)
+            else:
+                # Informing user how system understood him in case of voice and text processing
+                if notify_user is True:
+                    bot.send_message(message.chat.id, m2_result.message)
+
+                if m3_result.is_file is False:
+                    bot.send_message(message.chat.id, m3_result.number)
+                else:
+                    path = m3_result.path + '\\'
+                    bot.send_message(message.chat.id, m3_result.number)
+                    file1 = open(path + names[0], 'rb')
+                    file2 = open(path + names[1], 'rb')
+                    bot.send_document(message.chat.id, file1)
+                    bot.send_document(message.chat.id, file2)
+    except:
+        bot.send_message(message.chat.id, constants.ERROR_SERVER_DOES_NOT_RESPONSE)
+    """
+
+# polling cycle
+if __name__ == '__main__':
+
+    admin_id = (65305591, 164241807, 139653713)
+
+    # for _id in admin_id:
+    #     bot.send_message(_id, "ADMIN_INFO: Бот запушен")
+
+    e = None
+    count = 0
+
+    while True:
+        try:
+            # No more than 5 attempts for one exception
+            if count < 900:
+                count += 1
+                bot.polling(none_stop=True)
+            else:
+                # err_message = "ADMIN_INFO: Бот упал.\n\nERROR: '{}'.".format(e)
+                # for _id in admin_id:
+                #     bot.send_message(_id, err_message)
+                break
+        except Exception as e1:
+            os.popen("ipconfig /flushdns")
+            if type(e) is type(e1):
+                time.sleep(10)
+            else:
+                e = e1
+                count = 0
---
+++ m1_req.py
@@ -0,0 +1,181 @@
+import re
+import constants
+import datetime
+import random as rng
+from m1_work_class import quest
+
+list_of_int = []
+useless_word_in_sen = []
+
+key_words_quantity = len(constants.KEY_WORDS)
+
+
+# check if the string is convertible to integer type
+def represents_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+# remove extra symbols and split the string
+def simple_split(s):
+    s = s.lower()
+    s = re.sub(r'[^\w\s]', '', s)
+    s_splitted = s.split()
+    return s_splitted
+
+
+# check if user it trying to salute the bot or say 'How are you?'
+def hello_back(s):
+    for _ in simple_split(s):
+        if _ in constants.HELLO:
+            return constants.HELLO_ANSWER[rng.randint(0, len(constants.HELLO_ANSWER) - 1)]
+        elif _ in constants.HOW_ARE_YOU:
+            return constants.HOW_ARE_YOU_ANSWER[rng.randint(0, len(constants.HOW_ARE_YOU_ANSWER) - 1)]
+
+
+# Adaptive allowable mistake for distance between user word and key word
+def allowable_error(word):
+    allowable_inaccuracy = {
+        1: 0, 2: 0, 3: 1, 4: 2,
+        5: 2, 6: 3, 7: 3, 8: 3,
+        9: 4, 10: 5, 11: 5, 12: 5,
+        13: 5, 14: 5, 15: 6, 16: 6,
+        17: 6, 18: 6, 19: 6, 20: 6
+    }
+
+    length = len(word)
+    if length > 20:
+        return 6
+
+    return allowable_inaccuracy[length]
+
+
+# the Levenstein distance algorithm
+def distance(a: object, b: object) -> object:
+    """Calculates the Levenshtein distance between a and b."""
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current_row = range(n + 1)  # Keep current and previous row, not entire matrix
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            if a[j - 1] != b[i - 1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
+
+
+# return index of possible key word
+def check_the_territories(str_user):
+    minimum_value = key_words_quantity
+    index_of_the_most_likely_variant = 0
+    i = 0
+    for _ in constants.KEY_WORDS:
+        distance_between_input_and_table_data = distance(str_user, constants.KEY_WORDS[i])
+        if (distance_between_input_and_table_data < minimum_value and
+                    distance_between_input_and_table_data <= allowable_error(str_user)):
+            minimum_value = distance_between_input_and_table_data
+            index_of_the_most_likely_variant = i
+        i += 1
+
+    return index_of_the_most_likely_variant
+
+
+# return potential key word
+def main_place(s):
+    s = re.sub(r'[^\w\s]', '', s)
+    list1 = s.split()
+    for s in constants.USELESS_PILE_OF_CRAP:
+        if s in list1:
+            list1.remove(s)
+
+    i = 0
+    for _ in list1:
+        result = check_the_territories(list1[i])
+        i += 1
+        # TODO: abstraction6
+        for s in constants.KEY_WORDS[11:-11]:
+            if s == constants.KEY_WORDS[result]:
+                return s
+
+
+# parse the string into affordable form for Module 2
+def main_func(s):
+    s = re.sub(r'[^\w\s]', '', s)
+    list1 = s.split()
+    list_of_int = []
+    for i in range(len(list1)):
+        if represents_int(list1[i]):
+            list_of_int.append(list1[i])
+
+    for s in constants.USELESS_PILE_OF_CRAP:
+        if s in list1:
+            list1.remove(s)
+
+    for s in list_of_int:
+        if s in list1:
+            list1.remove(s)
+
+    user_req = quest()
+
+    i = 0
+    now_date = datetime.date.today()
+    for _ in list1:
+        result = check_the_territories(list1[i])
+
+        # TODO: abstraction4
+        if constants.KEY_WORDS[result] == 'плановый':
+            user_req.planned_or_actual = 'плановый'
+        if constants.KEY_WORDS[result] == 'фактический':
+            user_req.planned_or_actual = "фактический"
+        if constants.KEY_WORDS[result] == 'бюджет':
+            user_req.sector = 'бюджет'
+        if constants.KEY_WORDS[result] == "доход":
+            user_req.subject = "доходы"
+        if constants.KEY_WORDS[result] == "расход":
+            user_req.subject = "расходы"
+        if constants.KEY_WORDS[result] == "дефицит":
+            user_req.subject = "дефицит"
+        if constants.KEY_WORDS[result] == 'текущий':
+            user_req.year = now_date.year
+            user_req.planned_or_actual = 'текущий'
+        if constants.KEY_WORDS[result] == 'прошлый':
+            user_req.year = now_date.year - 1
+
+        for s in constants.KEY_WORDS[11:-11]:
+            if s == constants.KEY_WORDS[result]:
+                user_req.place = s
+
+        if constants.KEY_WORDS[result] == 'налоговые':
+            user_req.sector = 'налоговый'
+        if constants.KEY_WORDS[result] == 'неналоговые':
+            user_req.sector = 'неналоговый'
+
+        for s in constants.KEY_WORDS[-11:]:
+            if s == constants.KEY_WORDS[result]:
+                user_req.sector = str(result - (key_words_quantity - 13))
+
+        i += 1
+    if user_req.sector == "":
+        user_req.sector = "null"
+    if user_req.planned_or_actual == "":
+        user_req.planned_or_actual = "null"
+    if user_req.place == "":
+        user_req.place = "null"
+    if user_req.year == 0 or user_req.year == now_date.year:
+        if len(list_of_int) != 0:
+            user_req.year = int(list_of_int[0])
+        else:
+            user_req.year = "null"
+
+    user_r = [user_req.subject, user_req.place.lower(), user_req.year, user_req.sector, user_req.planned_or_actual]
+    return user_r
